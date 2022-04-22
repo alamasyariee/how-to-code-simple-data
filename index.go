0c8782c915b4dc6ca5b16d303ee37144414b0854
alamasyariee <alamasyariee@gmmail.com>
2022-04-22 14:36:40 +0700

    Change structure file

--- index.go
+++ index.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-
 func main() {
-	// var num int
-
-	// fmt.Println(("Masukkan angka yang ingin diperiksa:"))
-	// fmt.Scanln(&num)
-
-	// IsPrime(num)
-
-  rand.Seed(time.Now().Unix())
-  var randomValue int
-
-  randomValue = RandomWithRange(2, 10)
-  fmt.Println("random number: ", randomValue);
-
-  randomValue = RandomWithRange(2, 10)
-  fmt.Println("random number: ", randomValue);
-
-  randomValue = RandomWithRange(2, 10)
-  fmt.Println("random number: ", randomValue);
 
 }
-
